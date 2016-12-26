332d41a2c63a2656660facdb2c435d72cfa0b434
Nian Liu <nliu@bol.com>
2016-12-26 18:52:29 +0100

    initialize project

---
+++ src/main/java/com/shengtian/service/TmcsServerApplication.java
@@ -0,0 +1,12 @@
+package com.shengtian.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmcsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmcsServerApplication.class, args);
+	}
+}
