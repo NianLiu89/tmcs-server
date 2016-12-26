0c295d317055f7b263df49a3744da0583abb26cd
Nian Liu <nliu@bol.com>
2016-12-26 19:10:58 +0100

    add paradox library and test with system out pu

--- src/test/java/com/shengtian/service/TmcsServerApplicationTests.java
+++ src/test/java/com/shengtian/service/TmcsServerApplicationTests.java
@@ -1,9 +1,15 @@
 package com.shengtian.service;
 
+import com.hxtt.sql.paradox.ParadoxDriver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +17,25 @@ public class TmcsServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void dummyLoadData() {
+		try {
+			Class.forName(ParadoxDriver.class.getName());
+			Connection e = DriverManager.getConnection("jdbc:paradox:/src/test/resources/data");
+			Statement statement = e.createStatement();
+			ResultSet resultSet = statement.executeQuery("select * from locality");
+
+			while(resultSet.next()) {
+				System.out.println("\n\n******************************");
+				System.out.println(resultSet.getObject("Gongwei"));
+				System.out.println(resultSet.getObject("Commentary"));
+				System.out.println(resultSet.getObject("moduleid"));
+			}
+		} catch (Exception var4) {
+			var4.printStackTrace();
+		}
 	}
 
 }
