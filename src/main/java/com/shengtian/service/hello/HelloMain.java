ba4f71f711e6db9f082b1378e3b4ab38966201c6
Nian Liu <nliu@bol.com>
2016-12-30 00:55:52 +0100

    get bean by name also works, and name is case sensitive

--- src/main/java/com/shengtian/service/hello/HelloMain.java
+++ src/main/java/com/shengtian/service/hello/HelloMain.java
@@ -7,6 +7,7 @@ public class HelloMain {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HelloConfig.class);
         HelloAnnotation obj = context.getBean(HelloAnnotation.class);
+//        HelloAnnotation obj = (HelloAnnotation) context.getBean("helloAnnotation");
         obj.setMessage1("hello");
         obj.setMessage2("annotation");
         obj.greet();
