4868527cd93856b30cb386e6e85f8bdff98dd39b
Nian Liu <nliu@bol.com>
2017-01-04 17:02:33 +0100

    aspect

---
+++ src/main/java/com/shengtian/service/dojo/aspect/AspectConfig.java
@@ -0,0 +1,11 @@
+package com.shengtian.service.dojo.aspect;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+
+
+}
---
+++ src/main/java/com/shengtian/service/dojo/aspect/AspectModule.java
@@ -0,0 +1,9 @@
+package com.shengtian.service.dojo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class AspectModule {
+
+
+}
