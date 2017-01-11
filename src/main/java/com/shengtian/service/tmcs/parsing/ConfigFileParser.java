7e5bc2c058d1d5d5dda2849abc516af4593203cb
Nian Liu <nliu@bol.com>
2017-01-11 16:54:26 +0100

    parse csv file

---
+++ src/main/java/com/shengtian/service/tmcs/parsing/ConfigFileParser.java
@@ -0,0 +1,4 @@
+package com.shengtian.service.tmcs.parsing;
+
+public class ConfigFileParser {
+}
--- src/test/java/com/shengtian/service/TmcsServerApplicationTests.java
+++ src/test/java/com/shengtian/service/TmcsServerApplicationTests.java
@@ -2,12 +2,14 @@ package com.shengtian.service;
 
 import com.hxtt.sql.paradox.ParadoxDriver;
 import com.shengtian.service.tmcs.data.init.Initializer;
+import com.shengtian.service.tmcs.parsing.CsvFileParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
+import java.io.IOException;
 import java.sql.*;
 
 @RunWith(SpringRunner.class)
@@ -17,43 +19,18 @@ public class TmcsServerApplicationTests {
 	@Inject
 	Initializer initializer;
 
+	@Inject
+	CsvFileParser csvFileParser;
+
 	@Test
 	public void contextLoads() {
 	}
 
 	@Test
-	public void dummyMain () {
-		initializer.initializeDataPoints();
+	public void dummyMain () throws IOException {
+		csvFileParser.parseCsvFile("data/data.csv");
+//		initializer.initializeDataPoints();
 	}
 
-	@Test
-	public void dummyLoadData() {
-		try {
-			Class.forName(ParadoxDriver.class.getName());
-			Connection e = DriverManager.getConnection("jdbc:paradox:/src/test/resources/data");
-			Statement statement = e.createStatement();
-			ResultSet resultSet = statement.executeQuery("select * from locality");
-			ResultSetMetaData metaData = resultSet.getMetaData();
-			for (int i = 0; i < metaData.getColumnCount(); i++) {
-				System.out.println(metaData.getColumnName(i+1));
-			}
-
-	int i = 0;
-			while(resultSet.next()) {
-				System.out.println("\n\n******************************");
-				System.out.println(resultSet.getObject("Gongwei"));
-				System.out.println(resultSet.getObject("Commentary"));
-				System.out.println(resultSet.getObject("moduleid"));
-				System.out.println(resultSet.getObject("kiln"));
-
-				i++;
-				if ( i  > 3) {
-					break;
-				}
-			}
-		} catch (Exception var4) {
-			var4.printStackTrace();
-		}
-	}
 
 }
